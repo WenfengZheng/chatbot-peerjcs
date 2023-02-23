14b36045fcec7e13db9618068722b3c997fac252
WenfengZheng <125814906+WenfengZheng@users.noreply.github.com>
2023-02-23 17:53:46 +0800

    Update GreedMatch.py

--- GreedMatch.py
+++ GreedMatch.py
@@ -3,7 +3,7 @@ import re
 
 
 def cosine_similarity(x, y, norm=False):
-    """ 计算两个向量x和y的余弦相似度 """
+    """ Computes the cosine similarity of two vectors x and y """
     assert len(x) == len(y), "len(x) != len(y)"
     zero_list = [0] * len(x)
     if x == zero_list or y == zero_list:
@@ -13,18 +13,18 @@ def cosine_similarity(x, y, norm=False):
     res = np.array([[x[i] * y[i], x[i] * x[i], y[i] * y[i]] for i in range(len(x))])
     cos = sum(res[:, 0]) / (np.sqrt(sum(res[:, 1])) * np.sqrt(sum(res[:, 2])))
 
-    return 0.5 * cos + 0.5 if norm else cos  # 归一化到[0, 1]区间内
+    return 0.5 * cos + 0.5 if norm else cos  # Normalized to [0, 1] interval
 
 
 def conver_float(x):
-    '''将词向量数据类型转换成可以计算的浮点类型'''
+    '''Convert the word vector data type to a floating point type that can be calculated'''
     float_str = x
     return [float(f) for f in float_str]
 
 
 def process_wordembe(path):
     '''
-    将词向量文件中的所有词向量存放到一个列表lines里
+    Store all word vectors in the word vector file into a list lines
     :param path: a path of english word embbeding file 'glove.840B.300d.txt'
     :return: a list, element is a 301 dimension word embbeding, it's form like this
             ['- 0.12332 ... -0.34542\n', ', 0.23421 ... -0.456733\n', ..., 'you 0.34521 0.78905 ... -0.23123\n']
@@ -36,7 +36,7 @@ def process_wordembe(path):
 
 def word2vec(x, lines):
     '''
-    将一个字符串(这里指句子）中所有的词都向量化，并存放到一个列表里
+    Vectorize all the words in a string (a sentence in the study) and store them in a list
     :param x: a sentence/sequence, type is string, for example 'hello, how are you ?'
     :return: a list, the form like [[word_vector1],...,[word_vectorn]], save per word embbeding of a sentence.
     '''
@@ -45,23 +45,23 @@ def word2vec(x, lines):
     for w in x:
         for line in lines:
             # print(line)
-            if w == line.split()[0]:  # 将词向量按空格切分到一个列表里，将列表的第一个词与x的word比较
+            if w == line.split()[0]:  # Split the word vector into a list by spaces, and compare the first word of the list with the word of x
                 print(w)
-                x_words.append(conver_float(line[:-1].split()[1:]))  # 若在词向量列表中找到对应的词向量，添加到x_words列表里
+                x_words.append(conver_float(line[:-1].split()[1:]))  # If the corresponding word vector is found in the word vector list, add it to the x_words list
                 break
     return x_words
 
 
 def greedy(x, x_words, y_words):
     '''
-    上面提到的第一个公式
+    The first equation mentioned above
     :param x: a sentence, type is string.
     :param x_words: list[list1, list2,...,listn], listk(k=1...n) is word vector which from sentence x,
     :param y_words: list[list1, list2,..., listn], listk(k=1...n) is word vector which from sentence y,
     :return: a scalar, it's value is in [0, 1]
     '''
-    cosine = []  # 存放一个句子的一个词与另一个句子的所有词的余弦相似度
-    sum_x = 0  # 存放最后得到的结果
+    cosine = []  # Stores the cosine similarity between a word in one sentence and all words in another sentence
+    sum_x = 0  # store the final result
     for x_v in x_words:
         for y_v in y_words:
             cosine.append(cosine_similarity(x_v, y_v))
@@ -74,7 +74,7 @@ def greedy(x, x_words, y_words):
 
 def greedy_match(path, x, y):
     '''
-    上面的第二个公式
+    The second equation above
     :param lines: english word embbeding list, like[['-','0.345',...,'0.3123'],...]
     :param x: a sentence, here is a candidate answer
     :param y: a sentence, here is reference answer
@@ -96,8 +96,8 @@ if __name__ == '__main__':
     # print(cosine_similarity([1, 1], [0, 0]))   # 0.0
     # print(cosine_similarity([1, 1], [-1, -1]))  # -1.0
     # print(cosine_similarity([1, 1], [2, 2]))  # 1.0
-    f = open('G:\\PycharmProjects\\test\\glove.840B.300d.txt', 'r', encoding='utf-8')  # 这里改成你自己项目的路径
-    path = 'G:\\PycharmProjects\\test\\glove.840B.300d.txt'  # 这里改成你自己项目的路径
+    f = open('G:\\PycharmProjects\\test\\glove.840B.300d.txt', 'r', encoding='utf-8')  # Change here to the path of your own project
+    path = 'G:\\PycharmProjects\\test\\glove.840B.300d.txt'  # Change here to the path of your own project
 
     # lines = process_wordembe(path)
     # print(lines[:1])
